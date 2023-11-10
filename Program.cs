588ad7dcaef2e754b3e69c88a7e3dd5b545b4415
Ulisses Moraes <ulissesmoraes74@gmail.com>
2023-11-10 19:47:49 -0400

    Fila - FIFO - QUEUE

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Queue<int> fila = new Queue<int>();
+
+fila.Enqueue(02);
+fila.Enqueue(04);
+fila.Enqueue(06);
+fila.Enqueue(08);
+fila.Enqueue(10);
+fila.Enqueue(12);
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+}
